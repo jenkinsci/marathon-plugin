f96036f9f9f4fa958dcb3e236f88f83324b8940b
Colin C <colin@mesosphere.com>
2016-11-28 13:38:03 -0800

    Remove Override in AuthenticationException
    Small code clean-up: remove override of toString that is not doing
    anything but calling super.toString().

--- src/main/java/com/mesosphere/velocity/marathon/exceptions/AuthenticationException.java
+++ src/main/java/com/mesosphere/velocity/marathon/exceptions/AuthenticationException.java
@@ -5,9 +5,4 @@ public class AuthenticationException extends Exception {
     public AuthenticationException(String message) {
         super(message);
     }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 }
